7a73e138c0a71331385ea41e0bacdd0cb096a238
Emanuele Santoruvo <emanuele64550@gmail.com>
2025-06-24 19:59:33 +0200

    Finalizzazione test

--- GCOV/Codice/main.c
+++ GCOV/Codice/main.c
@@ -4,7 +4,7 @@
 #include "CatalogoVideogame.h"
 
 int main(){
-    ScriviCatalogo("catalogo.dat"); // Creazione del file catalogo.dat con i videogiochi iniziali
+    ScriviCatalogo("Catalogo.dat"); // Creazione del file catalogo.dat con i videogiochi iniziali
     MenuPrincipale(); //Attivazione del menu principale per la gestione del catalogo
     return 0;
 }
